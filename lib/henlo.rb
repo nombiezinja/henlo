33b4ddbdeebc4b4efd2b15fbe285cf856356f34b
nombiezinja <tianyizhang1987@gmail.com>
2018-01-22 15:47:21 -0800

    -

--- lib/henlo.rb
+++ lib/henlo.rb
@@ -5,8 +5,20 @@ require "henlo/engine"
 
 module Henlo
 
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
   def self.hi_there
     puts "this is a test"
+  end 
+
+  module ClassMethods
+
+    def henlo_again 
+      puts "henlo again"
+    end 
+    
   end 
 
 end
