d09f168d6db59738ca5a778cf9b03129ce5e2c29
nombiezinja <tianyizhang1987@gmail.com>
2018-01-22 16:25:07 -0800

    -

--- lib/henlo/refreshable.rb
+++ lib/henlo/refreshable.rb
@@ -3,7 +3,11 @@ module Henlo::Refreshable
   def self.say_henlo(name)
     puts "henlo #{name}"
   end 
-
+  
+  def self.it_works 
+    puts "it works"
+  end 
+  
   def self.generate_henlo(user)
     puts ENV['RAILS_ENV']
   end 
